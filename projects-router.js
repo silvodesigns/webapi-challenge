86df69a014a6bdc0eb56af1cfb5574e0bbd6704f
Kevin-S <kevsilvo@gmail.com>
2019-08-18 20:13:49 -0400

    added second router

--- projects-router.js
+++ projects-router.js
@@ -3,6 +3,7 @@ const express = require("express");
 const router = express.Router();
 
 const projects = require('./data/helpers/projectModel.js');
+const actions = require('./data/helpers/actionModel.js');
 
 
 
@@ -38,7 +39,7 @@ router.get('/',(req,res)=>{
   });
  
  
-  router.post('/',(req,res)=>{
+  router.post('/:id',(req,res)=>{
     
      projects.insert(req.body)
      .then(response => {
@@ -109,6 +110,105 @@ router.get('/',(req,res)=>{
      })
   
   });
+
+  router.get('/actions/:id',(req,res)=>{
+    const { id } = req.params;
+    projects.getProjectActions(id)
+    .then(response => {
+        if(response){
+            res.status(200).json(response);
+         } else {b
+             res.status(404).json({
+                 message: "The project with the specified ID does not exist"
+             })
+         }
+    })
+    .catch(err=>{
+        res.status(400).json({message: err})
+    })
+ 
+ });
+
+
+ router.post('/actions/:id',(req,res)=>{
+
+    const { id } = req.params;
+    const newAction = req.body;
+    if(!id){
+        res.status(400).json({message:"The project with this ID dont exist"})
+    }
+    if(!newAction.project_id){
+        res.status(400).json({message:"Please add a project_id property"})
+
+    }
+    if(!newAction.description){
+        res.status(400).json({message:"Please add a description property"})
+
+    }
+    if(!newAction.notes){
+        res.status(400).json({message:"Please add a note property"})
+
+    }
+    actions.insert(newAction)
+    .then(response => {
+      
+        res.status(200).json(response);
+    })
+    .catch(err=>{
+        res.status(400).json({message: err})
+    })
+ 
+ });
+
+
+ router.put('/actions/:id',(req,res)=>{
+
+    const { id } = req.params;
+    const changes = req.body;
+  
+    actions.update(id,changes)
+    .then(response => {
+        if(id){
+        res.status(200).json(response);
+        } else{
+            return null;
+        }
+    })
+    .catch(err=>{
+        res.status(400).json({message: err})
+    })
+ 
+ });
+
+
+
+
+ router.put('/actions/:id',(req,res)=>{
+
+    const { id } = req.params;
+    const changes = req.body;
+  
+    actions.delete(id)
+    .then(response => {
+        if(id){
+        res.status(200).json(response);
+        } else{
+            return null;
+        }
+    })
+    .catch(err=>{
+        res.status(400).json({message: err})
+    })
+ 
+ });
+
+
+
+
+ 
+ 
+ 
+
 
   module.exports = router;
   
